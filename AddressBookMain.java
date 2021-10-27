2cf058c9cededfaa9c69f1d890c59d4c54ff3dda
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-27 13:31:22 +0530

    Add contact to address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,10 +1,14 @@
 package com.addressbook;
 
-//calling main method
 public class AddressBookMain {
+
 	public static void main(String[] args) {
-		AddressBook addressBook = new AddressBook("Amol", "Nagose", "chandanagar", "hyderabad", "Telangana", 500050,"16164606403161");
-		System.out.println(addressBook);
-	  
+	
+		System.out.println("Welcome to Address Book Program");
+		
+		AddressBook.newContact(0);
+		System.out.println(AddressBook.contacts[0]);
+
 	}
+
 }
